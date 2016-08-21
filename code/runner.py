6417edba83d6823f599dfae8b3867797151109ad
furio <francesco@furio.me>
2016-08-21 16:24:55 +0200

    ...

--- code/runner.py
+++ code/runner.py
@@ -1,30 +1,26 @@
 # Import the driver.
 import psycopg2
+import uuid
 
-# Connect to the "bank" database.
-conn = psycopg2.connect(database='teststuff', user='testuser', host='roach1', port=26257)
+def runApp():
+    conn = psycopg2.connect(database='teststuff', user='testuser', host='roach1', port=26257)
+    createTable(conn)
+    putStuff(conn)
+    conn.close()
 
-
-def runApp():  
-    print "Yoh"
-
-def runTest():
-    # Make each statement commit immediately.
+def createTable(conn):
     conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS fakedata(id string primary key, data1 string not null, data2 string, data3 string, data4 string, data5 string, data6 string)")
+    cur.close()
 
-    # Open a cursor to perform database operations.
+def putStuff(conn):
+    conn.set_session(autocommit=True)
     cur = conn.cursor()
 
     # Insert two rows into the "accounts" table.
-    cur.execute("INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)")
-
-    # Print out the balances.
-    cur.execute("SELECT id, balance FROM accounts")
-    rows = cur.fetchall()
-    print('Initial balances:')
-    for row in rows:
-        print([str(cell) for cell in row])
+    for i in xrange(10000):
+        cur.execute("INSERT INTO fakedata (id, data1, data2, data3, data4, data5) VALUES ('"+str(i)+"-"+str(uuid.uuid4())+","+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"')")
 
     # Close the database connection.
     cur.close()
-    conn.close()    
