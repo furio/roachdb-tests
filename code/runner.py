701bd714757459ba307a7540cf74309260c35d5f
furio <francesco@furio.me>
2016-08-27 11:09:17 +0200

    timing

--- code/runner.py
+++ code/runner.py
@@ -1,11 +1,19 @@
 # Import the driver.
 import psycopg2
 import uuid
+import timeit
 
 def runApp():
     conn = psycopg2.connect(database='teststuff', user='testuser', host='roach1', port=26257)
     createTable(conn)
+    start_time = timeit.default_timer()
     putStuff(conn)
+    elapsed = timeit.default_timer() - start_time
+    print elapsed
+    start_time = timeit.default_timer()
+    putStuffTrans(conn)
+    elapsed = timeit.default_timer() - start_time
+    print elapsed
     conn.close()
 
 def createTable(conn):
@@ -24,3 +32,22 @@ def putStuff(conn):
 
     # Close the database connection.
     cur.close()
+
+def putStuffTrans(conn):
+    conn.set_session(autocommit=False)
+
+    onestmt(conn, "SAVEPOINT cockroach_restart")
+
+    try:
+        with conn.cursor() as cur:
+            for i in xrange(10000):
+                cur.execute("INSERT INTO fakedata (id, data1, data2, data3, data4, data5) VALUES ('"+str(i)+"-"+str(uuid.uuid4())+","+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"','"+str(uuid.uuid4())+"')")
+
+        onestmt(conn, "RELEASE SAVEPOINT cockroach_restart")
+    except psycopg2.OperationalError as e:
+        print "Nope!"
+        onestmt(conn, "ROLLBACK TO SAVEPOINT cockroach_restart")
+
+def onestmt(conn, sql):
+    with conn.cursor() as cur:
+        cur.execute(sql)
